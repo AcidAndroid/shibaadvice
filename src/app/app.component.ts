097270de54f70800465d9fa82e62f79c8dbe7372
kelldintch <kelldintch@hotmail.com>
2018-04-21 14:51:53 -0500

    error logueo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
+// import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase/app";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  // ,template: `<ul>
+  //   <li *ngFor="let item of items | async">
+  //     {{ item.name }}
+  //   </li>
+  // </ul>`
 })
 export class AppComponent {
   title = 'ShibAdvice!';
   home_img = "http://cdn.shibe.online/shibes/f85fd66a23fc66754b68f2fac431e2fd0f773293.jpg"
   loged=false;
+  user: Observable<firebase.User>;
 
-  Login(){
-    alert('Logueado. Bienvenido')
+  login(){
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
     this.loged=true;
   }
+
+  // items: Observable<any[]>;
+  constructor(private afAuth:AngularFireAuth ){
+    //this.user = this.afAuth.authState;
+  }
+  // constructor(db: AngularFirestore) {
+  //   this.items = db.collection('items').valueChanges();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppComponent } from './app.component';
+
+const FIREBASECONF ={
+  apiKey: "AIzaSyCV7EKKtFnHXZ1Ia0MhlDpeJcIvhzhU2zc",
+    authDomain: "shibaadvice.firebaseapp.com",
+    databaseURL: "https://shibaadvice.firebaseio.com",
+    projectId: "shibaadvice",
+    storageBucket: "shibaadvice.appspot.com",
+    messagingSenderId: "388090866362"
+
+}
 
 
 @NgModule({
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(FIREBASECONF),
+    // AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
